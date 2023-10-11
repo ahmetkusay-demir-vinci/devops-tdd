865c1a16e76a01e789e0fb26a9d3c3d7ca1d287c
ahmetkusay-demir-vinci <128645290+ahmetkusay-demir-vinci@users.noreply.github.com>
2023-10-11 21:48:05 +0200

    refactor: externalise the instanciation of the Main class

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,11 +1,10 @@
 import { Main } from '../src/app.js';
 
 describe("A suite of test for the demo function of the main class", function(){
+    let main = new Main();
+    
     // Écrire un test
     it("fizzBuzz function should return 1 when given 1", function() {
-        // Arrange (On prépare ce dont on a besoin pour exécuter le test)
-        let main = new Main();
-
         // Act (On exécute la fonction qu'on veut tester)
         let result = main.fizzByzz(1);
 
@@ -13,15 +12,11 @@ describe("A suite of test for the demo function of the main class", function(){
         expect(result).toBe(1);
     })
     it("fizzBuzz function should return 2 when given 2", function() {
-        let main = new Main();
-
         let result = main.fizzByzz(2);
 
         expect(result).toBe(2);
     })
     it("fizzBuzz function should return fizz when given 3", function() {
-        let main = new Main();
-
         let result = main.fizzByzz(3);
 
         expect(result).toBe("fizz");
