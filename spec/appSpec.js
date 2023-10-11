fbd690b8801da0a551ac87abf352e95754b59d0b
ahmetkusay-demir-vinci <128645290+ahmetkusay-demir-vinci@users.noreply.github.com>
2023-10-11 21:35:44 +0200

    add: manage case 2

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -2,7 +2,7 @@ import { Main } from '../src/app.js';
 
 describe("A suite of test for the demo function of the main class", function(){
     // Écrire un test
-    it("fizzBuzz function return 1 when given 1", function() {
+    it("fizzBuzz function should return 1 when given 1", function() {
         // Arrange (On prépare ce dont on a besoin pour exécuter le test)
         let main = new Main();
 
@@ -11,5 +11,12 @@ describe("A suite of test for the demo function of the main class", function(){
 
         // Assert (On vérifie le résultat attendu)
         expect(result).toBe(1);
+    })
+    it("fizzBuzz function should return 2 when given 2", function() {
+        let main = new Main();
+
+        let result = main.fizzByzz(2);
+
+        expect(result).toBe(2);
     })
 });
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 export class Main {
     fizzByzz(number) {
+        if(number == 2) return 2;
         return 1;
     }
 }
